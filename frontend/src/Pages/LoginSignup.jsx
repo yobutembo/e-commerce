7492d9e6bd29db9dd1f25b8852912afc22168169
Yobu <121264075+yobutembo@users.noreply.github.com>
2024-09-27 14:56:30 +0530

    Url Update

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -4,79 +4,126 @@ import "./CSS/LoginSignup.css";
 const LoginSignup = () => {
   const [state, setState] = useState("Login");
   const [formData, setFormData] = useState({
-    username:"",
-    password:"",
-    email:""
-  })
+    username: "",
+    password: "",
+    email: "",
+  });
 
   const changeHandler = (e) => {
-    setFormData({...formData, [e.target.name]:e.target.value})
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const login = async () => {
-    console.log("login function executed", formData)
-    try{
-      const response = await fetch('http://localhost:4000/login',{
-        method: 'POST',
-        headers:{
-          Accept:'application/form-data',
-          'Content-Type': 'application/json'
-        },
-        body:JSON.stringify(formData)
-      })
-      const responseData = await response.json()
-  
-      if (responseData.success){
-        localStorage.setItem('auth-token', responseData.token)
-        window.location.replace('/')
-      }else{
-        alert(responseData.errors)
+    console.log("login function executed", formData);
+    try {
+      const response = await fetch(
+        "hhttps://js-clothing-backend.onrender.com/login",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/form-data",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const responseData = await response.json();
+
+      if (responseData.success) {
+        localStorage.setItem("auth-token", responseData.token);
+        window.location.replace("/");
+      } else {
+        alert(responseData.errors);
       }
-    }catch(error){
-      console.error("Error during login", error)
+    } catch (error) {
+      console.error("Error during login", error);
     }
-
-  }
+  };
   const signup = async () => {
-    console.log("signup function executed", formData)
-  try{
-    const response = await fetch('http://localhost:4000/signup',{
-      method: 'POST',
-      headers:{
-        Accept:'application/form-data',
-        'Content-Type': 'application/json'
-      },
-      body:JSON.stringify(formData)
-    })
-    const responseData = await response.json()
+    console.log("signup function executed", formData);
+    try {
+      const response = await fetch(
+        "https://js-clothing-backend.onrender.com/signup",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/form-data",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const responseData = await response.json();
 
-    if (responseData.success){
-      localStorage.setItem('auth-token', responseData.token)
-      window.location.replace('/')
-    }else{
-      alert(responseData.errors)
+      if (responseData.success) {
+        localStorage.setItem("auth-token", responseData.token);
+        window.location.replace("/");
+      } else {
+        alert(responseData.errors);
+      }
+    } catch (error) {
+      console.error("Error during signup", error);
     }
-  }catch(error){
-    console.error("Error during signup", error)
-  }
-  }
+  };
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state === "Sign Up" && <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Name" />}
-          <input type="email" name='email' value={formData.email} onChange={changeHandler} placeholder="Email Address" />
-          <input type="password" name='password' value={formData.password} onChange={changeHandler} placeholder="Password" />
+          {state === "Sign Up" && (
+            <input
+              name="username"
+              value={formData.username}
+              onChange={changeHandler}
+              type="text"
+              placeholder="Name"
+            />
+          )}
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+            placeholder="Email Address"
+          />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+            placeholder="Password"
+          />
         </div>
-        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+        <button
+          onClick={() => {
+            state === "Login" ? login() : signup();
+          }}
+        >
+          Continue
+        </button>
         {state === "Login" ? (
           <p className="loginsignup-login">
-            Don't have an account?<span onClick={()=>{setState("Sign Up")}}> Sign Up</span>
+            Don't have an account?
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              {" "}
+              Sign Up
+            </span>
           </p>
         ) : (
           <p className="loginsignup-login">
-            Already have an account?<span onClick={()=>{setState("Login")}}> Login</span>
+            Already have an account?
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              {" "}
+              Login
+            </span>
           </p>
         )}
         <div className="loginsignup-agree">
